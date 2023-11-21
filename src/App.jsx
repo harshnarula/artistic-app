128d368f37c26e4bc01853d9e1d3fe4ad13a2afa
harshnarula <91422112+harshnarula@users.noreply.github.com>
2023-11-21 13:41:47 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import MainPage from './components/MainPage'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <MainPage/>
+    </>
+  )
+}
+
+export default App
