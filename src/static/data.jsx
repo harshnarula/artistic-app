c309f3dff9f8b95409bce5a3e57bfc7f167ce69f
harshnarula <91422112+harshnarula@users.noreply.github.com>
2023-11-21 13:41:20 +0530

    Add files via upload

---
+++ src/static/data.jsx
@@ -0,0 +1,134 @@
+export const icons = [
+    {
+        id: 1,
+        img: 'home.svg',
+        title: 'Home',
+    },
+    {
+        id: 2,
+        img: 'notification.svg',
+        title: 'Notifications',
+    },
+    {
+        id: 3,
+        img: 'heart.svg',
+        title: 'Shop',
+    },
+    {
+        id: 4,
+        img: 'message (1).svg',
+        title: 'Conversation',
+    },
+    {
+        id: 5,
+        img: 'message.svg',
+        title: 'Wallet',
+    },
+    {
+        id: 6,
+        img: 'favorite.svg',
+        title: 'Subscription',
+    },
+    {
+        id: 7,
+        img: 'profile.svg',
+        title: 'My Profile',
+    },
+    {
+        id: 8,
+        img: 'setting.svg',
+        title: 'Settings',
+    },
+]
+
+
+
+
+export const main_content = [
+    {
+        id: 1,
+        name: 'Lara Leones',
+        profile: 'pexels-ali-pazani-2613260.png',
+        username: '@thewallart',
+        description: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. ',
+        image: 'pexels-humphrey-muleba-2045248.png'
+    },
+    {
+        id: 2,
+        name: 'Thomas J.',
+        profile: 'pexels-imad-clicks-9810659.png',
+        username: '@thecustomcreater',
+        description: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. ',
+        image: 'pexels-tobias-bjørkli-2236382.png'
+    },
+]
+
+
+export const profile_card = [
+    {
+        id: 1,
+        name: 'Thomas Edward',
+        username: '@thewildwithyou',
+        profile: 'pexels-anastasia-shuraeva-4406721.png',
+        background: 'pexels-ekaterina-12203460.png',
+    },
+    {
+        id: 2,
+        name: 'Chris Doe',
+        username: '@thewildwithyou',
+        profile: 'pexels-andrea-piacquadio-3771118.png',
+        background: 'pexels-genaro-servín-763210.png',
+    },
+    {
+        id: 3,
+        name: 'Emilie Jones',
+        username: '@thewildwithyou',
+        profile: 'pexels-antoni-shkraba-4442102.png',
+        background: 'pexels-pixabay-164455.png',
+    },
+    {
+        id: 4,
+        name: 'Jessica Williams',
+        username: '@thewildwithyou',
+        profile: 'pexels-antoni-shkraba-4442005.png',
+        background: 'pexels-steve-johnson-1269968.png',
+    },
+    {
+        id: 5,
+        name: 'Jessica Williams',
+        username: '@thewildwithyou',
+        profile: 'pexels-antoni-shkraba-4442005.png',
+        background: 'pexels-fiona-art-5022849.png',
+    },
+]
+
+export const painting_card = [
+    {
+        id: 1,
+        paint: 'pexels-pixabay-164455.png',
+        name: 'Modern Wall Decor Framed Painting',
+        price: '$199.99',
+        rating: 5
+    },
+    {
+        id: 2,
+        paint: 'Image 40.png',
+        name: 'Modern Wall Decor Framed Painting',
+        price: '$199.99',
+        rating: 5
+    },
+    {
+        id: 3,
+        paint: 'Image 39.png',
+        name: 'Modern Wall Decor Framed Painting',
+        price: '$199.99',
+        rating: 5
+    },
+    {
+        id: 4,
+        paint: 'pexels-max-vakhtbovych-6782342.png',
+        name: 'Modern Wall Decor Framed Painting',
+        price: '$199.99',
+        rating: 5
+    },
+]
